bb0bd5952a8754b38861b5e2a6272c1579965538
KylinWu <midnightkylin@gmail.com>
2019-01-08 17:55:23 +0800

    Add more solved problem (classic)
    206. Reverse Linked List
    Approach: 1. Iterative method
              2. Recursive method

---
+++ Algorithms/C/Linked-List/206-ReverseLinkedList--Iterative.c
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* curr = head, *prev = NULL, *next;
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    head = prev;
+    return head;
+}
